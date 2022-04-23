be0cf1822e00bee5f648967b6c0c6c963b793108
morning <v.shamraev1@yandex.ru>
2022-04-23 19:03:23 +0300

    search in profile

--- LearnCreator.py
+++ LearnCreator.py
@@ -13,15 +13,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
-user_now = ''
 lessons_now_count = 0
 
 
 def start_parameters():
-    return ['', 0, 5]
+    return ['', '', 0, 5]
 
 
-search_now, FirstLesson, limit = start_parameters()
+search_now, user_now, FirstLesson, limit = start_parameters()
 
 
 @login_manager.user_loader
@@ -68,10 +67,26 @@ def back():
     return redirect("/")
 
 
+@app.route('/limit_profile')
+def lim_profile():
+    global limit, FirstLesson
+    FirstLesson += 5
+    limit += 5
+    return redirect(f"/profile/{user_now}")
+
+
+@app.route('/back_profile')
+def back_profile():
+    global limit, FirstLesson
+    FirstLesson -= 5
+    limit -= 5
+    return redirect(f"/profile/{user_now}")
+
+
 @app.route('/main')
 def main():
-    global limit, FirstLesson, search_now
-    search_now, FirstLesson, limit = start_parameters()
+    global limit, FirstLesson, search_now, user_now
+    search_now, user_now, FirstLesson, limit = start_parameters()
     return redirect("/")
 
 
@@ -83,6 +98,16 @@ def delete_favourites(id):
     db_sess.delete(favourite)
     db_sess.commit()
     return redirect("/")
+
+
+@app.route('/delete_favourites_profile/<int:id>')
+def delete_favourites_profile(id):
+    db_sess = db_session.create_session()
+    user_id = current_user.id
+    favourite = db_sess.query(Favourites).filter(user_id == Favourites.user_id, id == Favourites.lesson_id).first()
+    db_sess.delete(favourite)
+    db_sess.commit()
+    return redirect(f"/profile/{user_now}")
 
 
 @app.route('/add_favourites/<int:id>')
@@ -98,14 +123,16 @@ def add_favourites(id):
 
 @app.route('/your_lessons')
 def your_les():
-    global profile
+    global profile, search_now, user_now, FirstLesson, limit
+    search_now, x, FirstLesson, limit = start_parameters()
     profile = 'your_lessons'
     return redirect(f"/profile/{user_now}")
 
 
 @app.route('/your_favourites')
 def your_fav():
-    global profile
+    global profile, search_now, user_now, FirstLesson, limit
+    search_now, x, FirstLesson, limit = start_parameters()
     profile = 'your_favourites'
     return redirect(f"/profile/{user_now}")
 
@@ -113,7 +140,7 @@ def your_fav():
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
     global limit, FirstLesson, search_now
-    search_now, FirstLesson, limit = start_parameters()
+    search_now, user_now, FirstLesson, limit = start_parameters()
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
@@ -144,7 +171,7 @@ def reqister():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     global limit, FirstLesson, search_now
-    search_now, FirstLesson, limit = start_parameters()
+    search_now, user_now, FirstLesson, limit = start_parameters()
     form = LoginForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
@@ -165,23 +192,44 @@ def logout():
     return redirect("/")
 
 
-@app.route('/profile/<string:name>')
+@app.route('/profile/<string:name>', methods=['GET', 'POST'])
 def watch_profile(name):
-    global limit, user_now, profile, FirstLesson, search_now
+    global limit, user_now, profile, FirstLesson, search_now, lessons_now_count
+    search = SearchForm()
     if name != user_now:
         profile = 'your_lessons'
-    search_now, FirstLesson, limit = start_parameters()
+        search_now, user_now, FirstLesson, limit = start_parameters()
+    if request.method == "POST":
+        FirstLesson = 0
+        limit = 5
+        search_now = search.search.data
     db_sess = db_session.create_session()
     user = db_sess.query(User).filter(User.name == name).first()
     user_now = user.name
     db_sess = db_session.create_session()
     if profile == 'your_lessons':
-        lessons = db_sess.query(Lesson).filter(Lesson.user_id == user.id).order_by(-Lesson.rate).limit(limit)
+        lessons = db_sess.query(Lesson).filter(Lesson.title.like(f'%{search_now}%') | Lesson.title.like(
+            '%' + str(search_now).capitalize() + '%') | Lesson.title.like(
+            '%' + str(search_now).lower() + '%') | Lesson.title.like(
+            '%' + str(search_now).upper() + '%'), Lesson.user_id == user.id).order_by(-Lesson.rate)[FirstLesson:limit]
+        lessons_now_count = db_sess.query(Lesson).filter(Lesson.title.like(f'%{search_now}%') | Lesson.title.like(
+            '%' + str(search_now).capitalize() + '%') | Lesson.title.like(
+            '%' + str(search_now).lower() + '%') | Lesson.title.like(
+            '%' + str(search_now).upper() + '%'), Lesson.user_id == user.id).count()
     else:
         favourites_ids = [x.lesson_id for x in
                           db_sess.query(Favourites.lesson_id).filter(Favourites.user_id == user.id).distinct()]
-        lessons = db_sess.query(Lesson).filter(Lesson.id.in_(favourites_ids)).order_by(-Lesson.rate).limit(limit)
-    return render_template('profile.html', user=user, profile=profile, lessons=lessons)
+        lessons = db_sess.query(Lesson).filter(Lesson.title.like(f'%{search_now}%') | Lesson.title.like(
+            '%' + str(search_now).capitalize() + '%') | Lesson.title.like(
+            '%' + str(search_now).lower() + '%') | Lesson.title.like(
+            '%' + str(search_now).upper() + '%'), Lesson.id.in_(favourites_ids)).order_by(-Lesson.rate)[
+                  FirstLesson:limit]
+        lessons_now_count = db_sess.query(Lesson).filter(Lesson.title.like(f'%{search_now}%') | Lesson.title.like(
+            '%' + str(search_now).capitalize() + '%') | Lesson.title.like(
+            '%' + str(search_now).lower() + '%') | Lesson.title.like(
+            '%' + str(search_now).upper() + '%'), Lesson.id.in_(favourites_ids)).count()
+    return render_template('profile.html', user=user, profile=profile, lessons=lessons, search=search,
+                           lessons_now_count=lessons_now_count, FirstLesson=FirstLesson, limit=limit)
 
 
 @app.route('/lesson/<int:id>')
